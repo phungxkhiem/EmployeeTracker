59da910e2f95500bab0fc726546bc09c2583fcc3
Khiem Thanh Phung <khiemphung@khiems-mbp.attlocal.net>
2023-01-29 12:19:01 -0800

    fixed db.js

---
+++ db.js
@@ -0,0 +1,32 @@
+const db = require("./connection")
+class DB {
+    constructor(connection) { this.connection = connection }
+
+    viewEmployees = () => {
+        return this.connection.promise().query("select * from employee")
+
+    }
+   
+    viewDepartments = () => {
+        return this.connection.promise().query("select * from department")
+
+    }
+   
+    addDepartment = (name) => {
+        return this.connection.promise().query("insert into department (name) values (?) ", name)
+
+    }
+
+    addRole = (name,salary,department) => {
+        return this.connection.promise().query("insert into role (title, salary, department_id) values (?, ?, ?) ", [name, salary, department])
+
+    }
+    addEmployee = (firstname, lastname,role, manager ) => {
+        return this.connection.promise().query("insert into employee (first_name, last_name, role_id, manager_id) values (?, ?, ?, ?) ", [firstname,lastname,role,manager])
+
+    }
+}
+
+
+
+module.exports = new DB(db)
